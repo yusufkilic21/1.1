651b9917ac551e356bc24649951345c4f37b4182
yusuf kilic <yusufkilic811@gmail.com>
2024-08-29 21:25:48 +0300

    selnıum1

---
+++ src/Gun03/_01_OpeningWebsite.java
@@ -0,0 +1,15 @@
+package Gun03;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _01_OpeningWebsite {
+    public static void main(String[] args) {
+
+        WebDriver driver =new ChromeDriver();
+                         //new firefoxdrıver
+                         //new edgeDriver
+
+
+    }
+}
