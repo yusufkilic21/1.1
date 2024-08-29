50a1b9a1da29b405f75ed34940b4228d1cf818a7
yusuf kilic <yusufkilic811@gmail.com>
2024-08-29 21:46:26 +0300

    selnıum1.1

--- src/Gun03/_01_OpeningWebsite.java
+++ src/Gun03/_01_OpeningWebsite.java
@@ -2,6 +2,8 @@ package Gun03;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class _01_OpeningWebsite {
     public static void main(String[] args) {
@@ -9,6 +11,18 @@ public class _01_OpeningWebsite {
         WebDriver driver =new ChromeDriver();
                          //new firefoxdrıver
                          //new edgeDriver
+
+        driver.get("https://techno.study/tr"); //web sayfasını ac
+
+        //3 sn bekle
+        try {
+            Thread.sleep(3000);
+
+        }catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver.quit(); //hafızadan temızle bosalt
 
 
     }
